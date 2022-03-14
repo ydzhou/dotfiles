f70d81686a84f22c3ada9861fb6e266d75d0076a
Yudi Zhou <yudi@Yudis-iMac.local>
2022-03-14 00:26:16 -0700

    clean up

--- mutt/gmail.c
+++
@@ -1,9 +0,0 @@
-source "gpg -d ~/.mutt/acc.c.gpg |"
-
-set folder = "imaps://imap.gmail.com:993"                     
-set spoolfile = "+INBOX"                                      
-set postponed = "+[Gmail]/Drafts"                             
-set header_cache = ~/.mutt/com.gmail.foo/cache/headers            
-set message_cachedir = ~/.mutt/com.gmail.foo/cache/bodies         
-set certificate_file = ~/.mutt/com.gmail.foo/certificates 
-set smtp_authenticators = 'gssapi:login'
