c4405c2f641af2b91f41fed308a7bb8171211e1b
Yudi Zhou <yd.zhou@me.com>
2022-03-26 12:01:10 -0700

    add nvim lua config

---
+++ nvim/lua/_plugins.lua
@@ -0,0 +1,40 @@
+local autopair = require('nvim-autopairs')
+autopair.setup {}
+
+local ntree = require('nvim-tree')
+ntree.setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+    view = {
+      number = true,
+      relativenumber = true,
+    },
+    filters = {
+      custom = { ".git" },
+    },
+}
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "maintained",
+    sync_install = false,
+    ignore_install = {"javascript"},
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+require'nvim-tree'.setup {
+    auto_close = true,
+    disable_netrw = true,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    sort_by = "name",
+    view = {
+        width = 40,
+        side = "left",
+        number = false,
+        relativenumber = false,
+    },
+}
