cf7e1191e869ebc8cefd91f3f0e8922816d97c8f
ydzhou <yd.zhou@me.com>
2024-02-11 04:54:08 -0800

    Remove vim configs

--- README.md
+++ README.md
@@ -2,12 +2,3 @@ dotfiles
 =======
 
 Configure Files for vim, zsh and etc.
-
-## Vim Plugins
-
-To install Plug:
-
-```
-curl -fLo ~/dotfiles/vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-```
--- setup.sh
+++ setup.sh
@@ -6,14 +6,11 @@ echo "setup dotfile configs from: ${CONFIGROOT}"
 
 mkdir -p $HOME/.config
 
-ln -sf $CONFIGROOT/vim $HOME/.vim
-ln -sf $CONFIGROOT/vim/vimrc $HOME/.vimrc
 ln -sf $CONFIGROOT/zshrc $HOME/.zshrc
 ln -sf $CONFIGROOT/zprofile $HOME/.zprofile
-ln -sf $CONFIGROOT/nvim $HOME/.config/
 ln -sf $CONFIGROOT/kitty $HOME/.config/
 
 mkdir -p $HOME/.config/fd
-ln -sf $CONFIGROOT/ignore $HOME/.config/fd/
+ln -sf $CONFIGROOT/fd/ignore $HOME/.config/fd/
 
 echo "setup completed"
